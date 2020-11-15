d71fa37453a1f27f01fa7b681b90ddce07857116
adrian7123 <bueno1dad@gmail.com>
2020-11-15 13:33:51 -0300

    mudando o projeto para mobx com mvc

--- garota_capa/lib/main.dart
+++ garota_capa/lib/main.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:garota_capa/Pages/HomePage.dart';
+import 'package:garota_capa/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomePage(),
-    );
-  }
-}
+void main() => runApp(ModularApp(module: AppModule()));
